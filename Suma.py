2dad4863fa811753df1ab7aa04e02e56da81ceef
Rapvgs <108778492+Rapvgs@users.noreply.github.com>
2024-09-18 22:04:22 -0600

    subida de archivo

---
+++ Suma.py
@@ -0,0 +1,11 @@
+
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
+
+suma = numero1 + numero2
+
+#sumar los numeros
+suma = numero1 + numero2
+
+#resultado
+print(f"La suma es: {suma}")
