b095c3f7ba1d654347e1e6744851758b035227e9
Rapvgs <108778492+Rapvgs@users.noreply.github.com>
2024-09-18 22:16:59 -0600

    archivo modificado

--- Suma.py
+++ Suma.py
@@ -1,11 +1,9 @@
 
 numero1 = float(input("Ingrese el primer número: "))
 numero2 = float(input("Ingrese el segundo número: "))
+numero3 = float(input("Ingrese el tercer número: "))
+suma = numero1 + numero2 + numero3
 
-suma = numero1 + numero2
-
-#sumar los numeros
-suma = numero1 + numero2
 
 #resultado
 print(f"La suma es: {suma}")
